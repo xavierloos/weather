1d87766352f150460d540ba887bd14049a430a08
Javier Lopez <javierlopez@MacBook-Pro.home>
2020-07-21 20:20:18 +0100

    Handleling errors (try-catch)

--- src/components/weatherEngine.js
+++ src/components/weatherEngine.js
@@ -3,6 +3,8 @@ import WeatherCard from "./WeatherCard/component";
 
 const WeatherEngine = ({ location }) => {
     const [query, setQuery] = useState("");
+    const [error, setError] = useState(false);
+    const [loading, setLoading] = useState(false);
     const [weather, setWeather] = useState({
         temp: null,
         city: null,
@@ -13,18 +15,26 @@ const WeatherEngine = ({ location }) => {
     // Connecting to the weather API
     // AWAIT word help to wait for the data to pass to the var
     const getWeather = async q => {
-        const apiRes = await fetch(
-            `http://api.openweathermap.org/data/2.5/weather?q=${q}&units=metric&APPID=9ec9591a31e3be7446a43513c920d793`
-        );
-        // Transfer the database to a json file
-        const resJSON = await apiRes.json();
-        // return the data in a json file
-        setWeather({
-            temp: resJSON.main.temp,
-            city: resJSON.name,
-            condition: resJSON.weather[0].main,
-            country: resJSON.country
-        });
+        setQuery('');
+        setLoading(true);
+        try {
+            const apiRes = await fetch(
+                `http://api.openweathermap.org/data/2.5/weather?q=${q}&units=metric&APPID=9ec9591a31e3be7446a43513c920d793`
+            );
+            // Transfer the database to a json file
+            const resJSON = await apiRes.json();
+            // return the data in a json file
+            setWeather({
+                temp: resJSON.main.temp,
+                city: resJSON.name,
+                condition: resJSON.weather[0].main,
+                country: resJSON.country
+            });
+        } catch (error) {
+            setError(true);
+
+        }
+        setLoading(false);
     };
     const handleSearch = e => {
         e.preventDefault();
@@ -36,16 +46,28 @@ const WeatherEngine = ({ location }) => {
 
     return (
         <div>
-            <WeatherCard
-                temp={weather.temp}
-                condition={weather.condition}
-                city={weather.city}
-                country={weather.country}
-            />
-            <form>
-                <input value={query} onChange={e => setQuery(e.target.value)} />
-                <button onClick={e => handleSearch(e)}>Search</button>
-            </form>
+            {!loading && !error ? (
+                <div>
+                    <WeatherCard
+                        temp={weather.temp}
+                        condition={weather.condition}
+                        city={weather.city}
+                        country={weather.country}
+                    />
+                    <form>
+                        <input value={query} onChange={e => setQuery(e.target.value)} />
+                        <button onClick={e => handleSearch(e)}>Search</button>
+                    </form>
+                </div>
+            ) : loading ? (
+                <div style={{ color: 'black' }}>Loading</div>
+            ) : !loading & error ? (
+                <div style={{ color: 'black' }}>
+                    There has been an error<br />
+                    <button onClick={() => setError(false)}>Reset</button>
+                </div>
+            ) : null}
+
         </div>
     );
 };
